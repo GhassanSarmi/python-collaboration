ffe28958f1a41cb73e5bf8f54175523a829ad5e8
GhassanSarmi <alsarmi.g@northeastern.edu>
2025-04-14 22:02:31 -0400

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -1,1 +1,11 @@
+# View all tasks
+    def view_tasks(self):
+        if not self.tasks:  # Check if there are no tasks
+            print("No tasks to display.")
+        else:
+            # Print each task with its status (Completed or Pending)
+            for i, task in enumerate(self.tasks, start=1):
+                status = "Completed" if task["completed"] else "Pending"
+                print(f"{i}. {task['task']} [{status}]")
 
+put code here
